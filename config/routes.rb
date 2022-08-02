3f08e05586564e1962e62d59689d009b3e875e20
Gina Mirando <gina.mirando5@gmail.com>
2022-08-02 06:55:21 -0500

    added 5 RESTful API routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get "/recipes" => "recipes#index"
+  post "/recipes" => "recipes#create"
+  get "/recipes/:id" => "recipes#show"
+  patch "/recipes/:id" => "recipes#update"
+  delete "/recipes/:id" => "recipes#destroy"
 end
